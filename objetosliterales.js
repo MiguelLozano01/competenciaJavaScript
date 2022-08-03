950b1f543f18c0b9f9899431c67b3f66a79f9d0a
MiguelLozano01 <110314899+MiguelLozano01@users.noreply.github.com>
2022-08-02 20:53:10 -0500

    Create objetosliterales.js

---
+++ objetosliterales.js
@@ -0,0 +1,10 @@
+var numeros;{
+    numerador1 = 2;
+    numerador2=  Math.round(Math.random()*100);
+    denominador1 = 2;
+    denominador2=  Math.round(Math.random()*100);
+}
+var operacion1;
+for (let i = 0; i < 1; i++) {
+   operacion1=numeros.numerador1/numeros.denominador1;
+}
