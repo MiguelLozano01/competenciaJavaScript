8e94a05475234d4b8912f46fe9eace53c807c260
MiguelLozano01 <110314899+MiguelLozano01@users.noreply.github.com>
2022-08-03 07:23:38 -0500

    Update objetosliterales.js

--- objetosliterales.js
+++ objetosliterales.js
@@ -1,12 +1,15 @@
-var numeros;{
-    numerador1 = 2;
-    numerador2=  Math.round(Math.random()*100);
-    denominador1 = 2;
-    denominador2=  Math.round(Math.random()*100);
+var numeros = {
+    numerador1: 2,
+    numerador2:  Math.round(Math.random()*100),
+    denominador1: 2,
+    denominador2:  Math.round(Math.random()*100)
 }
 var operacion1;
+var operacion2;
 for (let i = 0; i < 1; i++) {
    operacion1=numeros.numerador1/numeros.denominador1;
+   operacion2 = numeros.numerador2/numeros.denominador2;
 }
 
-console.log(`Division numero uno es igual a ${operacion1}`)
+console.log(`Division numero uno es igual a ${operacion1}`);
+console.log(`Division numero uno es igual a ${operacion2}`);
